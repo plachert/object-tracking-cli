cae5646c360c94c81eb487dd1792f335382fefc5
plachert <pmlachert@gmail.com>
2024-04-19 13:00:06 +0200

    Add kdtree

--- object_tracking_cli/cli.py
+++ object_tracking_cli/cli.py
@@ -1,28 +1,32 @@
 import logging
 
-import click
-
 from .processing import process_video
+from .video_streaming import UnsupportedVideoFormat
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@click.command()
-@click.argument(
-    "video_file",
-    type=click.Path(
-        exists=False,
-        file_okay=True,
-        dir_okay=False,
-        readable=True,
-    ),
-    required=True,
-)
-@click.option(
-    "--use-kdtree",
-    is_flag=True,
-    help="Use KD-tree for tracking.",
-)
-def cli(video_file, use_kdtree):
-    process_video(video_file, use_kdtree)
+def cli():
+    while True:
+        video_path = input("Enter the path to the video file: ")
+        use_kdtree = (
+            True
+            if input("Use KD-Tree? Enter True or False: ").lower() == "true"
+            else False
+        )
+        try:
+            process_video(
+                video_path,
+                use_kdtree,
+            )  # wide error scope. Refactor. TODO: process_video(video_cap)
+        except FileNotFoundError as e:
+            logger.error(f"{e} Try again")
+        except UnsupportedVideoFormat as e:
+            logger.error(f"{e} Try again")
+        except AssertionError as e:
+            logger.error(f"{e} Try again")
+
+
+if __name__ == "__main__":
+    cli()
