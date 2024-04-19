58fb8d9e05201182cbf6f8f1ef3871d2ae1a68f6
plachert <pmlachert@gmail.com>
2024-04-19 11:45:08 +0200

    Add KD-tree approach

--- object_tracking_cli/object_tracking/trackers.py
+++ object_tracking_cli/object_tracking/trackers.py
@@ -3,9 +3,34 @@ from collections import OrderedDict
 from typing import List
 
 import numpy as np
+from scipy.spatial import KDTree
 from scipy.spatial import distance as dist
 
 from ..object_detection.detection import Bbox_xyxy
+
+
+def find_closest_pairs_kdtree(centroids, points):
+    centroid_tree = KDTree(centroids)
+    closest_centroids_indices = centroid_tree.query(points)[1]
+    used_centroid_idxs = set()
+    closest_pairs = {}
+    for point_idx, centroid_idx in enumerate(closest_centroids_indices):
+        # resolve potential conflict
+        if centroid_idx in used_centroid_idxs:
+            k = 2
+            while k < len(centroids):
+                next_centroid_idx = centroid_tree.query(points[point_idx], k=k)[1][
+                    k - 1
+                ]
+                if next_centroid_idx not in used_centroid_idxs:
+                    closest_pairs[point_idx] = next_centroid_idx
+                    used_centroid_idxs.add(next_centroid_idx)
+                    break
+                k += 1
+        else:
+            closest_pairs[point_idx] = centroid_idx
+            used_centroid_idxs.add(centroid_idx)
+    return closest_pairs
 
 
 class ObjectTracker(ABC):
@@ -32,36 +57,54 @@ class ObjectTracker(ABC):
         del self._objects[object_id]
         del self._missing_frames[object_id]
 
+    def handle_missing(self, object_id: int):
+        self._missing_frames[object_id] += 1
+        if self._missing_frames[object_id] >= self._max_missing_frames:
+            self.deregister_object(object_id)
+
     @abstractmethod
     def update(self, bboxes_xyxy: List[Bbox_xyxy]):  # noqa
         """Assign, register or deregister objects based on last detections."""
 
 
 class NaiveTracker(ObjectTracker):
-    def update(self, rects):
-        if len(rects) == 0:
-            to_deregister = []
-            for objectID in self._missing_frames.keys():
-                self._missing_frames[objectID] += 1
-                if self._missing_frames[objectID] > self._max_missing_frames:
-                    to_deregister.append(objectID)
-            for id_ in to_deregister:
-                self.deregister_object(id_)
-            return self._objects
-        inputCentroids = np.zeros((len(rects), 2), dtype="int")
-        for i, (startX, startY, endX, endY) in enumerate(rects):
-            cX = int((startX + endX) / 2.0)
-            cY = int((startY + endY) / 2.0)
-            inputCentroids[i] = (cX, cY)
+    def __init__(self, use_kdtree: bool = False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.use_kdtree = use_kdtree
 
-        if len(self._objects) == 0:
-            for i in range(0, len(inputCentroids)):
-                self.register_object(inputCentroids[i])
+    def _bboxes_to_centroids(self, bboxes: List[Bbox_xyxy]):
+        centroids = np.zeros((len(bboxes), 2), dtype="int")
+        for i, (x1, y1, x2, y2) in enumerate(bboxes):
+            center_x = int((x1 + x2) / 2.0)
+            center_y = int((y1 + y2) / 2.0)
+            centroids[i] = (center_x, center_y)
+        return centroids
 
+    def _handle_assignments(self, bbox_centroids, object_ids, centroids):
+        if self.use_kdtree:
+            matches = find_closest_pairs_kdtree(
+                centroids, bbox_centroids
+            )  # new point idx to centroid idx
+            used_centroids_idx = set()
+            used_bboxes_centroids_idx = set()
+            for bbox_centroid_idx, centroid_idx in matches.items():
+                self._objects[object_ids[centroid_idx]] = bbox_centroids[
+                    bbox_centroid_idx
+                ]
+                self._missing_frames[object_ids[centroid_idx]] = 0
+                used_centroids_idx.add(centroid_idx)
+                used_bboxes_centroids_idx.add(bbox_centroid_idx)
+            unused_centroids_idx = set(range(len(centroids))) - used_centroids_idx
+            unused_bboxes_centroids_idx = (
+                set(range(len(bbox_centroids))) - used_bboxes_centroids_idx
+            )
+            for unused_centroid_idx in unused_centroids_idx:
+                self.handle_missing(object_ids[unused_centroid_idx])
+            for unused_bbox_centroid_idx in unused_bboxes_centroids_idx:
+                self.register_object(bbox_centroids[unused_bbox_centroid_idx])
         else:
-            objectIDs = list(self._objects.keys())
-            objectCentroids = list(self._objects.values())
-            D = dist.cdist(np.array(objectCentroids), inputCentroids)
+            # https://pyimagesearch.com/2018/07/23/simple-object-tracking-with-opencv/ approach
+            D = dist.cdist(np.array(centroids), bbox_centroids)
             rows = D.min(axis=1).argsort()
             cols = D.argmin(axis=1)[rows]
 
@@ -72,8 +115,8 @@ class NaiveTracker(ObjectTracker):
                 if row in usedRows or col in usedCols:
                     continue
 
-                objectID = objectIDs[row]
-                self._objects[objectID] = inputCentroids[col]
+                objectID = object_ids[row]
+                self._objects[objectID] = bbox_centroids[col]
                 self._missing_frames[objectID] = 0
 
                 usedRows.add(row)
@@ -84,20 +127,37 @@ class NaiveTracker(ObjectTracker):
 
             if D.shape[0] >= D.shape[1]:
                 for row in unusedRows:
-                    objectID = objectIDs[row]
+                    objectID = object_ids[row]
                     self._missing_frames[objectID] += 1
 
                     if self._missing_frames[objectID] > self._max_missing_frames:
                         self.deregister_object(objectID)
-
             else:
                 for col in unusedCols:
-                    self.register_object(inputCentroids[col])
+                    self.register_object(bbox_centroids[col])
+
+    def update(self, bboxes):
+        # no new bounding boxes
+        if len(bboxes) == 0:
+            to_deregister = []
+            for objectID in self._missing_frames.keys():
+                self._missing_frames[objectID] += 1
+                if self._missing_frames[objectID] > self._max_missing_frames:
+                    to_deregister.append(objectID)
+            for id_ in to_deregister:
+                self.deregister_object(id_)
+            return self._objects
+
+        # get centriods from bboxes
+        bbox_centroids = self._bboxes_to_centroids(bboxes)
+
+        # no registered objects. Register all new bboxes
+        if len(self._objects) == 0:
+            for i in range(0, len(bbox_centroids)):
+                self.register_object(bbox_centroids[i])
+
+        else:
+            object_ids = list(self._objects.keys())
+            centroids = list(self._objects.values())
+            self._handle_assignments(bbox_centroids, object_ids, centroids)
         return self._objects
-
-
-if __name__ == "__main__":
-    tracker = NaiveTracker()
-    tracker.update([(1, 1, 2, 2), (5, 1, 6, 2)])
-    tracker.update([(4, 1, 5, 2), (6, 1, 7, 2)])
-    print(tracker.objects)
