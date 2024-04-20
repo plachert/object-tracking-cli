254948a7b69743d5513c0adc2dba4a1aa109d660
plachert <pmlachert@gmail.com>
2024-04-20 15:16:51 +0200

    Add multiple trackers, resize frame

--- object_tracking_cli/processing.py
+++ object_tracking_cli/processing.py
@@ -4,15 +4,30 @@ import cv2
 import seaborn as sns
 
 from .object_detection.detection import YOLODetector
-from .object_tracking.trackers import NaiveTracker
+from .object_tracking.trackers import NaiveTracker, ObjectTracker
 from .video_streaming import VideoStream
+from typing import Dict
+
+from .utils.image_utils import resize_with_aspect_ratio
 
 
-def process_frame(frame, detector, tracker):
+def process_frame(
+        frame, 
+        detector: YOLODetector, 
+        trackers: Dict[str, ObjectTracker], 
+        class_to_color_and_name,
+        ):
     bboxes_with_class = detector.predict(frame)
     bboxes = [bbox[:-1] for bbox in bboxes_with_class]
-    tracker.update(bboxes)
-    return {"bboxes": bboxes_with_class, "trackers": tracker}
+    processed_frames = []
+    for tracker_name, tracker in trackers.items():
+        frame_copy = frame.copy()
+        tracker.update(bboxes)
+        plot_bboxes(frame_copy, bboxes_with_class, class_to_color_and_name)
+        plot_tracking(frame_copy, tracker, tracker_name)
+        processed_frames.append(frame_copy)
+    processed_frame = cv2.hconcat(processed_frames)
+    return processed_frame
 
 
 def fps_to_interval(fps: float):
@@ -26,19 +41,24 @@ def wait_for_next_frame(desired_interval: float, start_time: float) -> float:
     return time.time()
 
 
-def plot_tracking(frame, tracker):
+def plot_tracking(frame, tracker, tracker_name):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.5
+    thickness = 2
+    color = (0, 0, 255)
+    cv2.putText(frame, tracker_name, (10, 30), font, font_scale, color, thickness)
     for object_id, (x, y) in tracker.objects.items():
         text = f"ID {object_id}"
         cv2.putText(
             frame,
             text,
             (x - 10, y - 10),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0, 255, 0),
-            2,
+            font,
+            font_scale,
+            color,
+            thickness,
         )
-        cv2.circle(frame, (x, y), 4, (0, 255, 0), -1)
+        cv2.circle(frame, (x, y), 4, color, -1)
 
 
 def make_class_to_color_and_name(class_id_to_name):
@@ -80,24 +100,29 @@ def process_video(video_path: str, use_kdtree: bool = False, desired_fps: float 
     object_detector = YOLODetector()
 
     # Setup Object Trackers
-    object_tracker = NaiveTracker(use_kdtree=use_kdtree)
+    object_trackers = {
+        "tracker 1": NaiveTracker(use_kdtree=True), 
+        "tracker 2": NaiveTracker(use_kdtree=False),
+    }
 
     # Setup class_to_color
-    class_to_color = make_class_to_color_and_name(object_detector.class_id_to_name)
+    class_to_color_and_name = make_class_to_color_and_name(object_detector.class_id_to_name)
 
     desired_interval = fps_to_interval(desired_fps)
     start_time = time.time()
     while True:
         start_time = wait_for_next_frame(desired_interval, start_time)
         frame = video_stream.get_last_frame()
+        frame = resize_with_aspect_ratio(frame, target_width=600)
         if frame is None:
             break
-        processed = process_frame(
-            frame, detector=object_detector, tracker=object_tracker
+        processed_frame = process_frame(
+            frame, 
+            detector=object_detector, 
+            trackers=object_trackers, 
+            class_to_color_and_name=class_to_color_and_name,
         )
-        plot_tracking(frame, processed["trackers"])
-        plot_bboxes(frame, processed["bboxes"], class_to_color)
-        cv2.imshow("Frame", frame)
+        cv2.imshow("Frame", processed_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             video_stream.stop()
             break
