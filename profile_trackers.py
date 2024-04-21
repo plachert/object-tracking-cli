9791aba0502709c7155b9af361268c3c9160540b
plachert <pmlachert@gmail.com>
2024-04-21 19:21:43 +0200

    Fix profiling

--- profile_trackers.py
+++ profile_trackers.py
@@ -1,17 +1,21 @@
-from object_tracking_cli.object_tracking.trackers import NaiveTracker
 import time
+
+from object_tracking_cli.object_tracking.trackers import MotionAgnosticTracker
 
 
 def perfect_move(n_objects: int = 10):
-    bboxes = [(0, 2*idx, 2, 2) for idx in range(n_objects)]
+    bboxes = [(0, 2 * idx, 2, 2, None, None) for idx in range(n_objects)]
     while True:
-        bboxes = [(bbox[0]+1, bbox[1], bbox[2]+1, bbox[3]) for bbox in bboxes]
+        bboxes = [
+            (bbox[0] + 1, bbox[1], bbox[2] + 1, bbox[3], None, None) for bbox in bboxes
+        ]
         yield bboxes
 
 
 if __name__ == "__main__":
-    tracker = NaiveTracker(use_kdtree=False)
-    kd_tracker = NaiveTracker(use_kdtree=True)
+    naive_tracker = MotionAgnosticTracker(assignment_strategy="naive")
+    kd_tracker = MotionAgnosticTracker(assignment_strategy="kd_tree")
+    hungarian_tracker = MotionAgnosticTracker(assignment_strategy="hungarian")
 
     def profile_tracker(tracker, n_objects=500, n_updates=100):
         start = time.time()
@@ -22,5 +26,6 @@ if __name__ == "__main__":
         end = time.time()
         return end - start
 
-    print(f"Regular tracker: {profile_tracker(tracker)}")
+    print(f"Naive tracker: {profile_tracker(naive_tracker)}")
     print(f"KD-tree tracker: {profile_tracker(kd_tracker)}")
+    print(f"Hungarian tracker: {profile_tracker(hungarian_tracker)}")
